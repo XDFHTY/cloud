7706bbb599f04ddde897274f69935bc8127912b9
XDFHTY <a1668281642@gmail.com>
2018-08-11 18:22:28 +0800

    修改pom eureka版本为1.4

---
+++ eureka_register_service_master/src/test/java/com/example/eureka_register_service_master/EurekaRegisterServiceMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.eureka_register_service_master;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaRegisterServiceMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
